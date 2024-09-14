5fca127363a87033fee81cbbf1265c56bdc8864e
Harrison Cramer <harrisoncramer@gmail.com>
2024-09-14 00:00:59 -0400

    Fixed build step

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 # Define an EC2 instance resource with specific attributes
 resource "aws_instance" "my_ec2" {
-  ami                    = "ami-0b947c5d5516fa06e" # ARM Archicture...
+  ami                    = "ami-0b947c5d5516fa06e" # ARM Architecture
   instance_type          = "t4g.nano"
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
